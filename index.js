ddd6295418073ac41dde1ea6e4b5605854eea9ce
Davidhrbchc <24046dh@creativehill.cz>
2025-05-29 18:05:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,12 +54,12 @@ document.addEventListener("DOMContentLoaded", () => {
   /* ZOBRAZENÍ SVG*/
   const isInViewport = (elem) => {
     const rect = elem.getBoundingClientRect();
-    return (
-      rect.top < window.innerHeight &&
-      rect.bottom > 0 &&
-      rect.left < window.innerWidth &&
-      rect.right > 0
-    );
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
   };
 
     /* LOADING SVG */
